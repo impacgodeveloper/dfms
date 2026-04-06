5e617ec66c11d23a3cebcf820afbad4128445f40
impacgodeveloper <impacgodeveloper@gmail.com>
2026-04-06 18:26:32 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2963567546"
+    serviceWorkerVersion: "3871736560"
   }
 });
